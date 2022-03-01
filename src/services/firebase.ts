2ab6e37cf8c3fe3960284ced113094b803ac77d6
Lauren Leslie <laurenlesliesilva@gmail.com>
2022-03-01 18:53:06 -0300

    Firebase credentials and imports

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,9 +1,13 @@
-// Import the functions you need from the SDKs you need
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+// import firebase from 'firebase/app';
 
-// Your web app's Firebase configuration
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth"
+import "firebase/compat/database"
+
+
+import 'firebase/auth';
+import 'firebase/database';
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -11,12 +15,10 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
 
-export const auth = firebase.auth();
-
-export const database = firebase.database();
+export const auth = firebase.auth()
+export const database = firebase.database()
