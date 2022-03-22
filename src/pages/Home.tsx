58335f7c220c928edcaa5bc4231a731d321fb5cb
Lauren Leslie <laurenlesliesilva@gmail.com>
2022-03-21 22:06:54 -0300

    first hook

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,8 +5,16 @@ import googleIcon from "../assets/images/google-icon.svg";
 import { Button } from "../components/Button";
 
 import '../styles/auth.scss';
+import { useHistory } from "react-router-dom";
 
 export function Home() {
+
+  const history = useHistory();
+  
+  function navigateToNewRoom() {
+    history.push("/rooms/new");
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -17,7 +25,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logo} alt="Let me ask" />
-          <button className="create-room">
+          <button onClick={navigateToNewRoom} className="create-room">
             <img src={googleIcon} alt="Google Icon" />
             Create your room with Google
           </button>
